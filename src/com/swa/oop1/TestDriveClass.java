f75a4d854869a7521e33a9eb19e882ee0215a135
chanakan <624259009@webmail.npru.ac.th>
2021-07-12 15:03:03 +0700

    rename variable and method

--- src/com/swa/oop1/TestDriveClass.java
+++ src/com/swa/oop1/TestDriveClass.java
@@ -5,7 +5,7 @@ public class TestDriveClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	        Product p = new Product("P0001","Nintendo Switch");
-	        System.out.println(p.getDetailProduct());
+	        System.out.println(p.getDetail());
 	    
 
 	}
